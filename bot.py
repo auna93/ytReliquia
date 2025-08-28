1d89df542b9860663e3e427d6bba1f11b68b2f20
Nicolas <auna93@live.cl>
2025-08-28 01:33:15 -0400

    Use cookies for yt-dlp and add .gitignore

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ YDL_OPTIONS = {
     'format': 'bestaudio/best',
     'noplaylist': True,
     'default_search': 'auto',
+    'cookiefile': 'cookies.txt',
 }
 FFMPEG_OPTIONS = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
